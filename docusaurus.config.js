47a59acbe3f96493bdc8f4f8a6b43a71792f6d72
taryune <taryunelab@gmail.com>
2023-03-14 13:21:55 +0100

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,170 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+
+/** @type {import('@docusaurus/preset-classic').Options} */ defaultSettings = {
+  remarkPlugins: [
+    [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
+    require('remark-math'),
+  ],
+  rehypePlugins: [require('rehype-katex')],
+};
+/**
+ * Defines a section with overridable defaults
+ * @param {string} section
+ * @param {import('@docusaurus/plugin-content-docs').Options} options
+ */
+function defineSection(section, options = {}) {
+  return [
+    '@docusaurus/plugin-content-docs',
+    /** @type {import('@docusaurus/plugin-content-docs').Options} */
+    ({
+      path: `docs/${section}`,
+      routeBasePath: section,
+      id: section,
+      sidebarPath: require.resolve('./sidebars.js'),
+      breadcrumbs: false,
+      editUrl: 'https://github.com/mycel-domain/mycel-docs',
+      ...defaultSettings,
+      ...options,
+    }),
+  ];
+}
+
+const SECTIONS = [
+  defineSection('overview'),
+];
+
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'mycel docs',
+  tagline: '',
+  url: 'https://docs.mycel.domains',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'mycel-domain', // Usually your GitHub org/user name.
+  projectName: 'mycel-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+    ...SECTIONS,
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: 'docs/home',
+          routeBasePath: '/home',
+          sidebarPath: require.resolve('./sidebars-home.js'),
+          breadcrumbs: false,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/mycel-domain/mycel-docs',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'mycel',
+        items: [
+          {
+            to: '/overview',
+            position: 'left',
+            label: 'Overview',
+          },
+          {
+            href: 'https://github.com/mycel-domain',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Product',
+            items: [
+              {
+                label: 'mycel',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/myceldomain',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/mycel-domain',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} mycel.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <main>
+        <div>
+          <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
+            <div className="w-11/12 sm:w-2/3 lg:flex  items-center flex-col mb-5 sm:mb-10">
+              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-black leading-7 md:leading-10">
+                Welcome to {siteConfig.title}
+              </h1>
+              <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
+                {siteConfig.tagline}
+              </p>
+            </div>
+            <a className="link-btn" href="overview">
+              Get started
+            </a>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
