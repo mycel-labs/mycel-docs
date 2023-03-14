16e8fbde020e9e06edd47de4ae61995b446cab0c
taryune <taryunelab@gmail.com>
2023-03-14 20:24:59 +0100

    add overview fig

--- docs/overview/index.md
+++ docs/overview/index.md
@@ -8,5 +8,7 @@ sidebar_position: 1
 
 mycel is a decentralized name service that can resolve domains and wallet addresses.
 mycel supports name resolution in DNS, IBC, and smart contracts on supported chains.  
-It is also integrated with other blockchain name services such as Ethereum Name Service (ENS).
+It is also integrated with other blockchain name services such as [Ethereum Name Service (ENS)](https://ens.domains).
 For example, by connecting your ENS domain to mycel, you can resolve your ENS domain (not only Ethereum addresses, but also Bitcoin, Cosmos, Aptos, and more) from other supported chains, or resolve your website by adding DNS records.
+
+![overview](../assets/overview.svg)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,13 @@ import Link from '@docusaurus/Link';
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Home" description="<head />">
       <main>
         <div>
           <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
             <div className="w-11/12 sm:w-2/3 lg:flex  items-center flex-col mb-5 sm:mb-10">
               <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-black leading-7 md:leading-10">
-                Welcome to {siteConfig.title}
+                🍄 {siteConfig.title} 🍄
               </h1>
               <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
                 {siteConfig.tagline}
