9710a1690f79086012793098b823634214da88f7
tarumi <taryunelab@gmail.com>
2023-09-14 13:52:23 +0200

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
   darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: 'white',
+      black: 'black',
+      primary: {
+        DEFAULT: 'var(--ifm-color-primary)',
+        light: 'var(--ifm-color-primary-light)',
+        dark: 'var(--ifm-color-primary-dark)',
+      },
+    },
+  },
+  extend: {
   },
   plugins: [],
 }
